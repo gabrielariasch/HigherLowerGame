d4127d73f5ba09ffb21ec0fbb763fecd3ccd6880
Gabriel Arias <132839839+gabrielariasch@users.noreply.github.com>
2023-06-15 18:17:15 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,35 +2,41 @@ import game_data
 import random
 import art
 
-game_on = True
 score = 0
+game_on = True
 dir = game_data.data
+
+def format_data(account):
+    account_name = account["name"]
+    account_desc = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_desc}, from {account_country}"
+
+def check_guess(guess, a_followers, b_followers):
+    if a_followers > b_followers:
+        return guess == 'a'
+    else:
+        return guess == 'b'
 
 while game_on:
     random1 = random.choice(dir)
     random2 = random.choice(dir)
 
-    print(f"Current score: {score}")
     print(art.logo)
-    print(f"Compare A: {random1['name']}, {random1['description']}, from {random1['country']}")
+    print(f"Compare A: {format_data(random1)}")
     print(art.vs)
-    print(f"Against B: {random2['name']}, {random2['description']}, from {random2['country']}")
+    print(f"Against B: {format_data(random2)}")
 
-    guess = input("Who has more followers? Type 'A' or 'B': ")
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    random1_follower_count = random1["follower_count"]
+    random2_follower_count = random2["follower_count"]
 
-    if guess == 'A':
-        if random1['follower_count'] > random2['follower_count']:
-            score += 1
-        elif random1['follower_count'] == random2['follower_count']:
-            score += 1
-        else:
-            print(f"Your was was incorrect! Your final score is: {score}")
-            break
-    elif guess == 'B':
-        if random1['follower_count'] > random2['follower_count']:
-            print(f"Your was was incorrect! Your final score is: {score}")
-            break
-        elif random1['follower_count'] < random2['follower_count']:
-            score += 1
-        else:
-            score += 1
+    is_correct = check_guess(guess,random1_follower_count,random2_follower_count)
+
+    if is_correct:
+        score += 1
+        print(f"Correct answer! - Current score: {score}")
+    else:
+        print(f"That's wrong! Final score: {score}")
+        game_on = False
+        break
